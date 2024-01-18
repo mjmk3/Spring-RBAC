c9aa30d7f5f8f32be94e97ce6061d08055102d48
Mohamad J Makki <mohamad.j.makki@gmail.com>
2024-01-18 03:48:14 +0300

    Role Based Access Control

---
+++ src/main/java/app/rbac/rbac/Service/Impl/UserServiceImpl.java
@@ -0,0 +1,18 @@
+package app.rbac.rbac.Service.Impl;
+
+import app.rbac.rbac.Service.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author MJ Makki
+ * @version 1.0
+ * @license SkyLimits, LLC (<a href="https://www.skylimits.tech">SkyLimits, LLC</a>)
+ * @email m.makki@skylimits.tech
+ * @since long time ago
+ */
+
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+}
